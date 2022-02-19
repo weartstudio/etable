d5f5053d16d4d46d2a8fa3343a414d7809520750
weartstudio <balazs@weart.hu>
2022-02-19 10:20:10 +0100

    react app

--- components/footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-	return (
-		<footer className='bg-dark text-white py-4'>
-			<div className='container'>
-				<p className='text-center small m-0'>Copyright</p>
-			</div>
-		</footer>
-	);
-}
