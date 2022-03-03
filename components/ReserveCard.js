77fb2f5b6191c5d7bab7c68288dd40878ffad343
weartstudio <balazs@weart.hu>
2022-03-03 14:37:06 +0100

    részletek

--- components/ReserveCard.js
+++ components/ReserveCard.js
@@ -1,49 +1,23 @@
 import React from 'react'
-import fetch from 'isomorphic-unfetch';
-import { useRouter } from 'next/router';
-import { Col, Card, Stack, Badge, Button } from 'react-bootstrap';
 import Text from '../components/low-level/Text';
 
 
 export default function FoglalasCard( { book } ) {
-	const router = useRouter();
 
-	const deleteThis = async (id) => {
-    try {
-        const deleted = await fetch(`/api/books/${id}`, {
-            method: "Delete"
-        });
-        router.push("/");
-    } catch (error) {
-        console.log(error)
-    }
-  }
 
 	return (
-		<Col>
-			<Card className='shadow-sm bg-white'>
-				<Card.Header>
-					<Stack direction="horizontal" className='justify-content-between'>
-						<span>{book.name}</span>
-						<Badge>{book.time}</Badge>
-					</Stack>				
-				</Card.Header>
-				<Card.Body>
-					<Stack gap={4} direction='horizontal' className='small text-muted'>
-						<Text text={book.people} icon="bi-people" />
-						<Text text={book.tel} icon="bi-telephone" />
-						<Text text={book.email} icon="bi-envelope" />
-						<Button 
-							size='sm' 
-							variant='outline-danger' 
-							className='ms-auto'
-							onClick={() => deleteThis(book._id)}
-						>
-							<i className='bi bi-trash'></i>
-						</Button>
-					</Stack>
-				</Card.Body>
-			</Card>
-		</Col>
+		<div className='col'>
+			<div className='card shadow-sm bg-white'>
+				<div className='card-header d-flex align-items-center justify-content-between'>
+					<a href={`/${book._id}`}>{book.name}</a>
+					<Text text={book.time} tag="badge bg-primary" />
+				</div>
+				<div className='card-body d-flex small text-muted'>
+					<Text text={book.people} icon="bi-people" tag="me-3"/>
+					<Text text={book.tel} icon="bi-telephone" tag="me-3"/>
+					<Text text={book.email} icon="bi-envelope" />					
+				</div>
+			</div>
+		</div>
 	)
 }
