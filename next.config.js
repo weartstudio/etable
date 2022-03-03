2b2a2ab50a21be6bd6411c1e9f96b52a6965ccd8
weartstudio <balazs@weart.hu>
2022-03-03 12:27:10 +0100

    my site url

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   env: {
       MONGO_URI: "mongodb+srv://weart:weart@cluster0.qdkyq.mongodb.net/etable?retryWrites=true&w=majority",
       // MY_SITE_URI: "http://localhost:3000"
-      SITE_URI: "https://etable.hu"
+      MY_SITE_URI: "https://etable.hu"
   }
 }
