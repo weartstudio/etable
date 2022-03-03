9296871167e291415a4d2928546b0a0715046a21
weartstudio <balazs@weart.hu>
2022-03-03 12:02:21 +0100

    a

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  env: {
-      MONGO_URI: "mongodb+srv://weart:weart@cluster0.qdkyq.mongodb.net/etable?retryWrites=true&w=majority",
-      MY_SITE_URI: "http://localhost:3000"
-      // MY_SITE_URI: "https://etable.hu"
-  }
-}
