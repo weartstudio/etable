804336d0d20bd4016ab83038ea8a06e23ab585d7
weartstudio <balazs@weart.hu>
2022-03-03 12:54:29 +0100

    less bs

--- pages/uj-foglalas.js
+++ pages/uj-foglalas.js
@@ -2,12 +2,17 @@ import { useState,useEffect } from 'react'
 import fetch from 'isomorphic-unfetch';
 import { useRouter } from 'next/router';
 import PageTitle from "../components/PageTitle";
-import { Card, Row, Col, Form, Spinner, FloatingLabel, Button, Collapse } from "react-bootstrap";
+import { Form, FloatingLabel, Button } from "react-bootstrap";
 import { Formik } from 'formik';
 
 export default function NewBook() {
   const [open, setOpen] = useState(false);
+  const [openClass, setOpenClass] = useState('');
   const router = useRouter();
+
+  useEffect(() => {
+    open ? setOpenClass('d-block') : setOpenClass('d-none')
+  }, [open]);
 
   const createBook = async (values) => {
     try {
@@ -47,140 +52,133 @@ export default function NewBook() {
           isSubmitting,
         }) => (
           isSubmitting
-          ? <Spinner animation="border" />
+          ? <div className="spinner-border" role="status"></div>
           :
-            <Form className='col-lg-10 mx-auto' onSubmit={handleSubmit}>
-              <Card className="shadow-sm">
-                <Card.Body>
+            <form className='col-lg-10 mx-auto card card-body shadow-sm' onSubmit={handleSubmit}>
 
-                    <h5>Adatok</h5>
-                    <Row  md={2} className='g-4 mb-5'>
+              <h5>Adatok</h5>
+              <div className="row row-cols-1 row-cols-md-2 g-4 mb-5">
 
-                      <Col>
-                        <FloatingLabel label="Név">
-                          <Form.Control 
-                            required
-                            placeholder="Minta János" 
-                            name="name"
-                            value={values.name}
-                            onBlur={handleBlur}
-                            onChange={handleChange}
-                          />
-                        </FloatingLabel>
-                      </Col>
+                <div className="col">
+                  <FloatingLabel label="Név">
+                    <Form.Control 
+                      required
+                      placeholder="Minta János" 
+                      name="name"
+                      value={values.name}
+                      onBlur={handleBlur}
+                      onChange={handleChange}
+                      />
+                  </FloatingLabel>
+                </div>
 
-                      <Col>
-                        <FloatingLabel label="Hány fő?">
-                          <Form.Control 
-                          required
-                            type='number' 
-                            placeholder='2'
-                            name="people"
-                            value={values.people}
-                            onChange={handleChange}
-                          />
-                        </FloatingLabel>
-                      </Col>
+                <div className="col">
+                  <FloatingLabel label="Hány fő?">
+                    <Form.Control 
+                    required
+                      type='number' 
+                      placeholder='2'
+                      name="people"
+                      value={values.people}
+                      onChange={handleChange}
+                    />
+                  </FloatingLabel>
+                </div>
 
-                      <Col>
-                        <FloatingLabel label="Telefon">
-                          <Form.Control 
-                            required
-                            type='text' 
-                            placeholder='Telefonszám'
-                            name="tel"
-                            value={values.tel}
-                            onChange={handleChange}
-                          />
-                        </FloatingLabel>
-                      </Col>
+                <div className="col">
+                  <FloatingLabel label="Telefon">
+                    <Form.Control 
+                      required
+                      type='text' 
+                      placeholder='Telefonszám'
+                      name="tel"
+                      value={values.tel}
+                      onChange={handleChange}
+                    />
+                  </FloatingLabel>
+                </div>
 
-                      <Col>
-                        <FloatingLabel label="Email">
-                          <Form.Control 
-                            type='text' 
-                            placeholder='emailcím'
-                            name="email"
-                            value={values.email}
-                            onChange={handleChange}
-                          />
-                        </FloatingLabel>
-                      </Col>
+                <div className="col">
+                  <FloatingLabel label="Email">
+                    <Form.Control 
+                      type='text' 
+                      placeholder='emailcím'
+                      name="email"
+                      value={values.email}
+                      onChange={handleChange}
+                    />
+                  </FloatingLabel>
+                </div>
 
-                    </Row>
+              </div>
 
+              <h5>Időpont választás</h5>
+              <div className="row row-cols-2 mb-5">
 
-                    <h5>Időpont választás</h5>
-                    <Row className='mb-5'>
+                <div className='col'>
+                  <FloatingLabel label="Dátum">
+                    <Form.Control 
+                      type='date' 
+                      name='when'
+                      value={values.when}
+                      onChange={handleChange}
+                    />
+                  </FloatingLabel>
+                </div>
 
-                      <Col>
-                        <FloatingLabel label="Dátum">
-                          <Form.Control 
-                            type='date' 
-                            name='when'
-                            value={values.when}
-                            onChange={handleChange}
-                          />
-                        </FloatingLabel>
-                      </Col>
+                <div className='col'>
+                  <FloatingLabel label="Időszak">
+                    <Form.Select
+                      name="idoszak"
+                      value={values.idoszak}
+                      onChange={handleChange}
+                    > 
+                      <option value="1">Ebéd - 11:00 - 14:00</option>
+                      <option value="2">Délután - 14:00 - 18:00</option>
+                      <option value="3">Este - 18:00 - 21:30</option>
+                    </Form.Select>
+                  </FloatingLabel>
+                </div>
+              </div>
 
-                      <Col>
-                        <FloatingLabel label="Időszak">
-                          <Form.Select
-                            name="idoszak"
-                            value={values.idoszak}
-                            onChange={handleChange}
-                          > 
-                            <option value="1">Ebéd - 11:00 - 14:00</option>
-                            <option value="2">Délután - 14:00 - 18:00</option>
-                            <option value="3">Este - 18:00 - 21:30</option>
-                          </Form.Select>
-                        </FloatingLabel>
-                      </Col>
+              <div className={openClass}>
+                <FloatingLabel controlId="newNote" label="Megjegyzés">
+                  <Form.Control 
+                    as="textarea" 
+                    placeholder="pl.: Tortát hoznak" 
+                    style={{ height: '100px' }} 
+                    name="note"
+                    value={values.note}
+                    onChange={handleChange}
+                  />
+                </FloatingLabel>
+              </div>
 
-                    </Row>
+              <div className='row mt-4 justify-content-between'>
 
+                <div className='col-auto'>
+                  <Button
+                    onClick={() => setOpen(!open)}
+                    variant="light"
+                    className='w-100'
+                  >
+                    { !open ? "+ Megjezés hozzáadása" : "Mégse" }
+                  </Button>
+                </div>
 
+                <div className='col-auto'>
+                  <Button
+                    as="input"
+                    type="submit"
+                    value="Foglalás elküldése"
+                    className='w-100'
+                  />
+                </div>
 
-                    <Collapse in={open}>
-                      <FloatingLabel controlId="newNote" label="Megjegyzés">
-                        <Form.Control 
-                          as="textarea" 
-                          placeholder="pl.: Tortát hoznak" 
-                          style={{ height: '100px' }} 
-                          name="note"
-                          value={values.note}
-                          onChange={handleChange}
-                        />
-                      </FloatingLabel>
-                    </Collapse>
-
-                    <Row className='mt-4 justify-content-between'>
-
-                      <Col md={3}>
-                        <Button
-                          onClick={() => setOpen(!open)}
-                          variant="light"
-                          className='w-100'
-                        >
-                          { !open ? "+ Megjezés hozzáadása" : "Mégse" }
-                        </Button>
-                      </Col>
-
-                      <Col md={3}>
-                        <Button
-                          as="input"
-                          type="submit"
-                          value="Foglalás elküldése"
-                          className='w-100'
-                        />
-                      </Col>
-
-                    </Row>
+              </div>
           
-                </Card.Body>
-              </Card>
-            </Form>
+
+            </form>
         )}
         </Formik>
     </>
