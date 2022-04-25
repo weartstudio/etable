928a9790cdb40dfaddfcf6794d505945d3733c19
weartstudio <balazs@weart.hu>
2022-04-25 18:18:37 +0200

    style

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,7 @@
 import React, {useState} from 'react'
 import { setToken } from '../services/token'
-import { useNavigate } from 'react-router-dom'
-import logoAlt from '../assets/logoAlt.png' 
-import { Alert, Container, Form } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom'
+import { Alert, Button, Container, Form, Row, Col } from 'react-bootstrap';
 import Header from '../components/Header';
 
 function Login() {
@@ -36,14 +35,23 @@ function Login() {
 	return (
 		<>
 			<Header logout />
-			<Container className='my-5'>
-				<h1 className="h4 text-center mb-4">Belépés éttermeknek</h1> 
-				<Form onSubmit={handleSubmit} className='mx-auto card card-body shadow gap-3' style={{maxWidth: 400}}>
-					{badPassWord ? <Alert dismissible variant='danger' onClose={() => setBadPassWord(false)}>Hibás email cím vagy jelszó!</Alert> : '' }
-					<input onChange={(e)=>setName(e.target.value)} value={name} className='form-control' placeholder='email cím' type="text"  />
-					<input onChange={(e)=>setPass(e.target.value)} value={pass} className='form-control' placeholder='jelszó' type="password"/>
-					<input className='btn btn-primary px-4 mx-auto' type="submit" value="Belépés" />
-				</Form>
+			<Container className='my-5' style={{maxWidth: 1024}}>
+				<Row className='align-items-center g-5'>
+					<Col lg='4'>
+						<Form onSubmit={handleSubmit} className="bg-light p-4 rounded border">
+							<h2 className='h5 mb-4'>Belépés éttermek</h2>
+							{badPassWord ? <Alert dismissible variant='danger' onClose={() => setBadPassWord(false)}>Hibás email cím vagy jelszó!</Alert> : '' }
+							<input onChange={(e)=>setName(e.target.value)} value={name} className='form-control mb-3' placeholder='email cím' type="text"  />
+							<input onChange={(e)=>setPass(e.target.value)} value={pass} className='form-control mb-3' placeholder='jelszó' type="password"/>
+							<input className='btn btn-primary px-4 mx-auto' type="submit" value="Belépés" />
+						</Form>
+					</Col>
+					<Col>
+						<h1 className="h4 mb-4">Nincs még fiókod?</h1> 
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ut ultricies lorem. Phasellus efficitur diam at odio porta pharetra. Donec pharetra sagittis diam a lobortis.</p>
+						<Link to='/etable-ettermeknek'>Tovább olvasom...</Link>
+					</Col>
+				</Row>
 			</Container>
 		</>
 	)
