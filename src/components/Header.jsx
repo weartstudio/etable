781e302f376f8a6db7653eb7094fade30e7c7b99
weartstudio <balazs@weart.hu>
2022-04-25 09:45:53 +0200

    header mobile optimization

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,16 +6,20 @@ import logo from '../assets/logo.png'
 function Header() {
 
 	return (
-		<Navbar bg="dark" variant='dark' className='mb-5'>
+		<Navbar bg="dark" variant='dark' className='mb-5'  expand="lg">
 			<Container>
-				<Navbar.Brand href={'/'}><img src={logo} style={{maxHeight: 40}} /></Navbar.Brand>
-				<Nav className="ms-auto">
-					<Nav.Link href={'/reservations'}>Foglalások</Nav.Link>
-					<Nav.Link href={'/new-reservation'}>Új foglalás</Nav.Link>
-					<Nav.Link href={'/login'} onClick={ () => removeToken() }>
-						<i className="bi bi-box-arrow-right"></i>
-					</Nav.Link>
-				</Nav>
+				<Navbar.Brand href={'/'}><img src={logo} style={{maxHeight: 40}} alt='' /></Navbar.Brand>
+				<Navbar.Toggle aria-controls="etable-main-nav" />
+				<Navbar.Collapse id="etable-main-nav">
+					<Nav className="ms-auto">
+						<Nav.Link href={'/reservations'}>Foglalások</Nav.Link>
+						<Nav.Link href={'/new-reservation'}>Új foglalás</Nav.Link>
+						<Nav.Link href={'/login'} onClick={ () => removeToken() }>
+							<i className="bi bi-box-arrow-right"></i>
+							<span className='d-inline d-lg-none ms-2'>Kijelentkezés</span>
+						</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
 			</Container>
 		</Navbar>
 	)
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { setToken } from '../services/token'
 import { useNavigate } from 'react-router-dom'
 import logoAlt from '../assets/logoAlt.png' 
+import { Alert } from 'react-bootstrap';
 
 
 function Login() {
 	const navigate = useNavigate();
 
-	const [name,setName] = React.useState("");
-	const [pass,setPass] = React.useState("");
+	const [name,setName] = useState("");
+	const [pass,setPass] = useState("");
+	const [badPW,setBadPW] = useState(false);
 
 	function handleSubmit(e){
 		e.preventDefault();
+		setBadPW(false)
 
 		fetch("https://api.etable.hu/users/login", { 
 			method: 'POST', 
@@ -25,6 +28,8 @@ function Login() {
 				if (tartalom){
 					setToken(tartalom)
 					navigate("/reservations")
+				} else {
+					setBadPW(true) 
 				}
 			});
 	}
@@ -32,10 +37,11 @@ function Login() {
 	return (
 		<div className="container my-5">
 			<div className="text-center mb-4">
-				<img src={logoAlt} className="mx-auto" style={{maxHeight: 60}} />
+				<img src={logoAlt} className="mx-auto" style={{maxHeight: 60}} alt='' />
 			</div> 
 			<form  onSubmit={handleSubmit} className='mx-auto card card-body shadow gap-3' style={{maxWidth: 500}}>
-				<input onChange={(e)=>setName(e.target.value)} value={name} className='form-control' placeholder='név' type="text"  />
+				{badPW ? <Alert dismissible variant='danger' onClose={() => setBadPW(false)}>Hibás email cím vagy jelszó!</Alert> : '' }
+				<input onChange={(e)=>setName(e.target.value)} value={name} className='form-control' placeholder='email cím' type="text"  />
 				<input onChange={(e)=>setPass(e.target.value)} value={pass} className='form-control' placeholder='jelszó' type="password"/>
 				<input className='btn btn-primary px-4 mx-auto' type="submit" value="Belépés" />
 			</form>
